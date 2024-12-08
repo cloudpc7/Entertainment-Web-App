d6bc13a2f2703d69a5dc0099df5c5938e7feee40
cloudpc7 <cloudp7@hotmail.com>
2024-12-08 13:41:09 -0800

    scaffolding main sass structure

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import "../node_modules/bootstrap/scss/bootstrap.scss";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
